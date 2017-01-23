03939b3070ca33d7fff46da39c07804b616c7b08
David Hardy <david.hardy@codecentric.nl>
2017-01-24 00:33:03 +0100

    Reinstatiated copyright

--- rxfirebaseadmin/src/main/java/nl/endran/rxfirebaseadmin/DataSnapshotMapper.java
+++ rxfirebaseadmin/src/main/java/nl/endran/rxfirebaseadmin/DataSnapshotMapper.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2017 David Hardy
+ * Copyright 2016 renanferrari
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
